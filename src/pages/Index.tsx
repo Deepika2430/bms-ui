7b12f608de75f89a11c3dc47eb117d8dc57d1cd0
Abhi Kumar <abhi_yallapu@tecnics.com>
2025-02-26 23:47:47 +0530

    feat(Home): replace welcome message with BmsDashboard component

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,15 @@
+import BmsDashboard from "@/components/dashboard/Dashboard";
 
 const Home = () => {
   return (
     <div className="min-h-screen pt-32 px-4">
-      <div className="max-w-4xl mx-auto">
+        <BmsDashboard />
+      {/* <div className="max-w-4xl mx-auto">
         <h1 className="text-4xl font-bold text-nav-foreground mb-6">Welcome</h1>
         <p className="text-nav-muted text-lg">
           Navigate through the application using the menu above.
         </p>
-      </div>
+      </div> */}
     </div>
   );
 };
