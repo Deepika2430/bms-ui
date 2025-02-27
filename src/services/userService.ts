d5dfc369b5ca8ba8bc2672940a47f10b5ef93a52
Deepika2430 <deepika_pabbisetti@tecnics.com>
2025-02-27 20:42:05 +0530

    feat(UserService): implement user details fetching with error handling and response transformation

---
+++ src/services/userService.ts
@@ -0,0 +1,28 @@
+import config from "@/config";
+import { getToken } from "./authService";
+
+export const getUserDetails = async () => {
+    const token = getToken();
+    try {
+        const response = await fetch(`${config.apiBaseUrl}/user/details`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            },
+            redirect: "follow"
+        });
+        const userDetails = (await response.json());
+        return {
+            firstName: userDetails?.employee_details.first_name,
+            lastName: userDetails?.employee_details.last_name,
+            role: userDetails?.employee_details.role,
+            id: userDetails?.employee_details.id,
+            phone: userDetails?.employee_details.phone,
+            email: userDetails?.email,
+            departmentName: userDetails?.departments?.name,
+        }
+    }
+    catch (error) {
+        return error?.message;
+    }
+}
