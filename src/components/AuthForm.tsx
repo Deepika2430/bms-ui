f592bca31d588aa1fb9907f18c568b2367daccdd
Deepika Pabbisetti <deepika_pabbisetti@tecnics.com>
2025-02-27 12:56:18 +0530

    add navigation to close button

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -57,7 +57,7 @@ export default function AuthForm({ onClose }) {
       <div className="bg-white p-6 rounded-lg shadow-lg w-96 relative">
         {/* Close Button */}
         <button
-          onClick={onClose}
+          onClick={() => navigate("/")}
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
         >
           &times;
