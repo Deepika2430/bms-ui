faeaab5471161c7bd52f064c7600a6cd48679e93
Abhi Kumar Yallapu <abhi_yallapu@tecnics.com>
2025-02-28 18:02:05 +0530

    add notification service functions

--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -28,3 +28,68 @@ export const getNotifications = async () => {
         return error?.message;
     }
 }
+
+export const getNotificationsHistory = async () => {
+    const token = getToken();
+    try {
+        const response = await fetch(`${config.apiBaseUrl}/user/notifications/history`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            redirect: "follow"
+        });
+        const notifications= await response.json();
+        return notifications.map((notification) => {
+            return {
+                id: notification.id,
+                title: notification.message,
+                message: notification.message,
+                type: "",
+                read: notification.is_read,
+                createdAt: notification.created_at,
+            }
+        })
+    }
+    catch (error) {
+        return error?.message;
+    }
+}
+
+export const createNotification = async (userId: string, message: string) => {
+    const token = getToken();
+    try {
+        const response = await fetch(`${config.apiBaseUrl}/user/notification`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({user_id: userId, message}),
+            redirect: "follow"
+        });
+        return await response.json();
+    }
+    catch (error) {
+        return error?.message;
+    }
+}
+
+export const updateNotification = async (id: string) => {
+    const token = getToken();
+    try {
+        const response = await fetch(`${config.apiBaseUrl}/user/notification/${id}`, {
+            method: "PUT",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            redirect: "follow"
+        });
+        return await response.json();
+    }
+    catch (error) {
+        return error?.message;
+    }
+}
